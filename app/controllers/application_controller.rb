38949743737370303cdd53df3b68c6ba3f0f43ce
TiPang <397962092@qq.com>
2019-04-11 22:49:20 +0800

    Initialize Repository

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡Hola, Mundo!"
+  end
+  def goodbye
+    render html: "Goodbye, World!"
+  end
 end
